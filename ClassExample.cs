bf423190c62086223cc7ab84898da3e7b68b506f
Andrew <DrewYak7@google.com>
2012-10-23 14:08:46 +0400

    First Save

---
+++ ClassExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+namespace ReadXML
+{
+    class Example
+    {
+        private const String filename = "TrainsAndAnotherStuff.xml";
+
+        public static void Main()
+        {
+            XmlTextReader reader;
+            reader = new XmlTextReader(filename);
+
+            reader.WhitespaceHandling = WhitespaceHandling.None;
+
+            while (reader.Read())
+            {
+                Console.WriteLine("Name: {0}", reader.Name);
+                Console.WriteLine("Value: {0}", reader.Value);
+                Console.WriteLine("ValueType: {0}", reader.ValueType);
+            }
+        }
+    }
+}
